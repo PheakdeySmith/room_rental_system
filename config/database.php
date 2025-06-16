58eb468e3b157455bec46d199f30a1a2873da431
smith <pheakdeysmith>
2025-06-16 13:07:34 +0700

    update on tabler

--- config/database.php
+++ config/database.php
@@ -45,11 +45,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'room-rental-db-pheakdeysmith-0f2b.b.aivencloud.com'),
-            'port' => env('DB_PORT', '18027'),
-            'database' => env('DB_DATABASE', 'defaultdb'),
-            'username' => env('DB_USERNAME', 'avnadmin'),
-            'password' => env('DB_PASSWORD', 'AVNS_Q73THuQgx5JD4OeQeSY'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -58,7 +58,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => base_path('storage/ssl/ca.pem'),
+                // PDO::MYSQL_ATTR_SSL_CA => base_path('storage/ssl/ca.pem'),
             ]) : [],
         ],
 
